83967c1b4fd9ed74e5a7e12499df2a2e0a21e23d
Meet-Modi-27 <114226316+Meet-Modi-27@users.noreply.github.com>
2024-03-23 14:45:31 +0530

    Login Page ready.

--- app/src/main/java/com/example/parking_management/login.java
+++ app/src/main/java/com/example/parking_management/login.java
@@ -3,6 +3,7 @@ package com.example.parking_management;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class login extends AppCompatActivity {
 
--- app/src/main/java/com/example/parking_management/splash_screen.java
+++ app/src/main/java/com/example/parking_management/splash_screen.java
@@ -2,26 +2,52 @@ package com.example.parking_management;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class splash_screen extends AppCompatActivity {
+
+    Animation topAnim,bottomAnim;
+
+    ImageView imageView;
+    TextView title,slogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(splash_screen.this,login.class);
-                startActivity(intent);
-                finish();
+        imageView = findViewById(R.id.Logo);
+        title = findViewById(R.id.title);
+        slogan = findViewById(R.id.Slogan);
 
-            }
-        },800);
+        imageView.setAnimation(topAnim);
+        title.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(splash_screen.this,login.class);
+            Pair[] pairs = new Pair[2];
+            pairs[0] = new Pair<View,String>(imageView,"Parking_logo");
+            pairs[1] = new Pair<View,String>(title,"Parking_text");
+
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(this,pairs);
+            startActivity(intent,options.toBundle());
+
+        },2000);
     }
 }
